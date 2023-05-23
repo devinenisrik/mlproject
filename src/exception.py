4bc0ffde70ea1f52a335a2547723220ed59287e9
devinenisrik <devinenis.34@gmail.com>
2023-05-23 09:26:18 +0530

    eda

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+#simport logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() #3rd info required
